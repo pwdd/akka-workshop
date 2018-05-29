c101aebd48266e98691e4f13ae02476e4c38c638
Aina Pinto <aina@8thlight.com>
2018-05-29 15:34:12 -0500

    Add coordinated shutdown
    - instead of stopping the Main actor directly, run shutdown with a reason

--- src/main/scala/rocks/heikoseeberger/chakka/iam/Main.scala
+++ src/main/scala/rocks/heikoseeberger/chakka/iam/Main.scala
@@ -1,5 +1,7 @@
 package rocks.heikoseeberger.chakka.iam
 
+import akka.actor.CoordinatedShutdown
+import akka.actor.CoordinatedShutdown.Reason
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ActorSystem, Behavior, Terminated}
 import org.apache.logging.log4j.core.async.AsyncLoggerContextSelector
@@ -7,9 +9,12 @@ import org.apache.logging.log4j.scala.Logging
 import pureconfig.loadConfigOrThrow
 
 object Main extends Logging {
+  import akka.actor.typed.scaladsl.adapter._
 
   sealed trait Command
   final case class Config(accounts: Accounts.Config)
+
+  final case object TopLevelActorTerminated extends Reason
 
   def main(args: Array[String]): Unit = {
     sys.props += "log4j2.contextSelector" -> classOf[AsyncLoggerContextSelector].getName // async logging
@@ -28,6 +33,7 @@ object Main extends Logging {
       Behaviors.receiveSignal {
         case (_, Terminated(actor)) =>
           logger.error(s"Shutting down because $actor terminated")
+          CoordinatedShutdown(context.system.toUntyped).run(TopLevelActorTerminated)
           Behaviors.stopped
       }
     }
