076f13bef98885c4bf3ea293820172e6e8e0eb8d
Aina Pinto <aina@8thlight.com>
2018-05-30 12:19:23 -0500

    Add cluster configuration

--- src/main/scala/rocks/heikoseeberger/chakka/iam/Main.scala
+++ src/main/scala/rocks/heikoseeberger/chakka/iam/Main.scala
@@ -1,9 +1,12 @@
 package rocks.heikoseeberger.chakka.iam
 
 import akka.actor.CoordinatedShutdown.Reason
-import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
 import akka.actor.typed.{ActorSystem, Behavior, Terminated}
-import akka.actor.{CoordinatedShutdown, Scheduler}
+import akka.actor.{CoordinatedShutdown, Scheduler, ActorSystem => UntypedSystem}
+import akka.cluster.typed.{Cluster, SelfUp, Subscribe}
+import akka.persistence.cassandra.query.scaladsl.CassandraReadJournal
+import akka.persistence.query.PersistenceQuery
 import akka.stream.Materializer
 import akka.stream.typed.ActorMaterializer
 import org.apache.logging.log4j.core.async.AsyncLoggerContextSelector
@@ -14,7 +17,7 @@ object Main extends Logging {
   import akka.actor.typed.scaladsl.adapter._
 
   sealed trait Command
-  final case class Config(accounts: Accounts.Config, api: Api.Config)
+  final case class Config(accounts: Accounts.Config, authenticator: Authenticator.Config, api: Api.Config)
 
   final case object TopLevelActorTerminated extends Reason
 
@@ -27,27 +30,38 @@ object Main extends Logging {
     logger.info(s"${system.name} started")
   }
 
-  def apply(config: Config): Behavior[Command] = {
-
+  def apply(config: Config): Behavior[SelfUp] = {
     Behaviors.setup { context =>
+      Cluster(context.system).subscriptions ! Subscribe(context.self, classOf[SelfUp])
 
-      implicit val mat: Materializer = ActorMaterializer()(context.system)
-      implicit val scheduler: Scheduler = context.system.scheduler
-
-      val authenticator = context.spawn(Authenticator(), "authenticator")
-      context.watch(authenticator)
-
-      val accounts = context.spawn(Accounts(config.accounts, authenticator), "accounts")
-      context.watch(accounts)
-
-      Api(config.api, accounts, authenticator)
-
-      Behaviors.receiveSignal {
-        case (_, Terminated(actor)) =>
-          logger.error(s"Shutting down because $actor terminated")
-          CoordinatedShutdown(context.system.toUntyped).run(TopLevelActorTerminated)
-          Behaviors.same
+      Behaviors
+        .receiveMessage[SelfUp] { _ =>
+        logger.info(s"${context.system.name} joined cluster and is up")
+        onSelfUp(config, context)
+        Behaviors.empty
       }
+        .receiveSignal {
+          case (_, Terminated(actor)) =>
+            logger.error(s"Shutting down because $actor terminated")
+            CoordinatedShutdown(context.system.toUntyped).run(TopLevelActorTerminated)
+            Behaviors.same
+        }
     }
+  }
+
+  private def onSelfUp(config: Config, context: ActorContext[SelfUp]): Unit = {
+    implicit val untypedSystem: UntypedSystem = context.system.toUntyped
+    implicit val mat: Materializer = ActorMaterializer()(context.system)
+    implicit val scheduler: Scheduler = context.system.scheduler
+    implicit val readJournal: CassandraReadJournal = PersistenceQuery(untypedSystem)
+      .readJournalFor[CassandraReadJournal](CassandraReadJournal.Identifier)
+
+    val authenticator = context.spawn(Authenticator(config.authenticator), "authenticator")
+    context.watch(authenticator)
+
+    val accounts = context.spawn(Accounts(config.accounts), "accounts")
+    context.watch(accounts)
+
+    Api(config.api, accounts, authenticator)
   }
 }
