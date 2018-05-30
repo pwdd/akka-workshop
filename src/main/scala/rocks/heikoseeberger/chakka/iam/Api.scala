af981e83453e9b7bd83ea44ded3dc6bbeedf3f1d
Aina Pinto <aina@8thlight.com>
2018-05-29 19:19:27 -0500

    Add sign up capability

--- src/main/scala/rocks/heikoseeberger/chakka/iam/Api.scala
+++ src/main/scala/rocks/heikoseeberger/chakka/iam/Api.scala
@@ -2,13 +2,17 @@ package rocks.heikoseeberger.chakka.iam
 
 import akka.Done
 import akka.actor.CoordinatedShutdown.{PhaseServiceRequestsDone, PhaseServiceUnbind, Reason}
-import akka.actor.{ActorSystem, CoordinatedShutdown}
+import akka.actor.typed.ActorRef
+import akka.actor.typed.scaladsl.AskPattern.Askable
+import akka.actor.{ActorSystem, CoordinatedShutdown, Scheduler}
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.{Directives, Route}
-import akka.http.scaladsl.model.StatusCodes.OK
+import akka.http.scaladsl.model.StatusCodes.{BadRequest, Conflict, Created, OK}
 import akka.pattern.after
 import akka.stream.Materializer
+import akka.util.Timeout
 import org.apache.logging.log4j.scala.Logging
+import de.heikoseeberger.akkahttpcirce.ErrorAccumulatingCirceSupport
 
 import scala.concurrent.Future
 import scala.concurrent.duration.FiniteDuration
@@ -16,18 +20,22 @@ import scala.util.{Failure, Success}
 
 object Api extends Logging {
 
-  final case class Config(address: String, port: Int, requestDoneAfter: FiniteDuration)
+  final case class Config(address: String, port: Int, requestDoneAfter: FiniteDuration, askTimeout: FiniteDuration)
 
   final case object BindFailure extends Reason
 
-  def apply(config: Config)(implicit untypedSystem: ActorSystem, mat: Materializer) = {
+  final case class SignUp(username: String)
+
+  def apply(config: Config, accounts: ActorRef[Accounts.Command])
+           (implicit untypedSystem: ActorSystem, mat: Materializer): Unit = {
     import config._
     import untypedSystem.dispatcher
 
+    implicit val scheduler: Scheduler = untypedSystem.scheduler
     val shutdown = CoordinatedShutdown(untypedSystem)
 
     Http()
-      .bindAndHandle(route, address, port)
+      .bindAndHandle(route(accounts, askTimeout), address, port)
       .onComplete {
         case Failure(cause) =>
           logger.error(s"Shutting down because cannot bind to $address:$port", cause)
@@ -43,14 +51,35 @@ object Api extends Logging {
       }
   }
 
-  def route: Route = {
+  def route(accounts: ActorRef[Accounts.Command], askTimeout: FiniteDuration)
+           (implicit duration: Scheduler): Route = {
     import Directives._
+
+    import ErrorAccumulatingCirceSupport._
+    import io.circe.generic.auto._
+
+    implicit val timeout: Timeout = askTimeout
 
     pathPrefix("iam") {
       pathEnd {
         get {
           complete {
             OK
+          }
+        }
+      } ~
+      pathPrefix("accounts") {
+        import Accounts._
+        pathEnd {
+          post {
+            entity(as[SignUp]) {
+              case SignUp(username) =>
+                onSuccess(accounts ? createAccount(username)) {
+                  case UsernameInvalid => complete(BadRequest)
+                  case UsernameTaken => complete(Conflict)
+                  case _: AccountCreated => complete(Created)
+                }
+            }
           }
         }
       }
