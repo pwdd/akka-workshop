94e2e5f2f1fed24c3eb7d3bc8dc6ebf4c7b77af7
Aina Pinto <aina@8thlight.com>
2018-05-30 11:04:03 -0500

    Turn Accounts into a persistent behavior

--- src/main/scala/rocks/heikoseeberger/chakka/iam/Accounts.scala
+++ src/main/scala/rocks/heikoseeberger/chakka/iam/Accounts.scala
@@ -1,20 +1,15 @@
 package rocks.heikoseeberger.chakka.iam
 
-import akka.NotUsed
-import akka.actor.Scheduler
-import akka.actor.typed.scaladsl.AskPattern.Askable
-import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ActorRef, Behavior}
-import akka.stream.Materializer
-import akka.stream.scaladsl.{Flow, Framing}
-import akka.util.{ByteString, Timeout}
+import akka.persistence.typed.scaladsl.PersistentBehaviors.CommandHandler
+import akka.persistence.typed.scaladsl.{Effect, PersistentBehaviors}
 import org.apache.logging.log4j.scala.Logging
 
-import scala.concurrent.duration.FiniteDuration
 import scala.util.matching.Regex
 
 object Accounts extends Logging {
   sealed trait Command
+  sealed trait Event
   final case class CreateAccount(username: String,
                                  password: String,
                                  replyTo: ActorRef[CreateAccountReply]) extends Command
@@ -23,44 +18,47 @@ object Accounts extends Logging {
   final case object UsernameTaken extends CreateAccountReply
   final case object UsernameInvalid extends CreateAccountReply
   final case object PasswordInvalid extends CreateAccountReply
-  final case class AccountCreated(username: String) extends CreateAccountReply
+  final case class AccountCreated(username: String) extends CreateAccountReply with Event
 
   final case class Config(usernameRegex: Regex, passwordRegex: Regex)
 
-  def apply(config: Config,
-            authenticator: ActorRef[Authenticator.Command],
-            usernames: Set[String] = Set.empty): Behavior[Command] = {
-    import config._
+  final case class State(usernames: Set[String] = Set.empty)
 
-    Behaviors.receiveMessage {
-      case CreateAccount(username, password, replyTo) =>
-        if (usernames contains username) {
-          replyTo ! UsernameTaken
-          Behaviors.same
-        } else if (!usernameRegex.pattern.matcher(username).matches) {
-          replyTo ! UsernameInvalid
-          Behaviors.same
-        } else if (!passwordRegex.pattern.matcher(password).matches) {
-          replyTo ! PasswordInvalid
-          Behaviors.same
-        } else {
-          authenticator ! Authenticator.AddCredentials(username, Passwords.createHash(password))
-          replyTo ! AccountCreated(username)
-          logger.info(s"Account for $username created")
-          Accounts(config, authenticator, usernames + username)
-        }
-    }
+  final val PersistenceId = "accounts"
+
+  def apply(config: Config,
+            authenticator: ActorRef[Authenticator.Command]): Behavior[Command] =
+    PersistentBehaviors.receive(PersistenceId,
+      State(),
+      commandHandler(config, authenticator),
+      eventHandler)
+
+  def commandHandler(config: Config,
+                     authenticator: ActorRef[Authenticator.Command]): CommandHandler[Command, Event, State] = {
+    case (_, State(usernames), CreateAccount(username, password, replyTo)) =>
+      if (usernames contains username) {
+        replyTo ! UsernameTaken
+        Effect.none
+      } else if (!config.usernameRegex.pattern.matcher(username).matches) {
+        replyTo ! UsernameInvalid
+        Effect.none
+      } else if (!config.passwordRegex.pattern.matcher(password).matches) {
+        replyTo ! PasswordInvalid
+        Effect.none
+      } else {
+        val accountCreated = AccountCreated(username)
+        Effect
+          .persist(accountCreated)
+          .andThen {
+            logger.info(s"Account for $username created")
+            authenticator ! Authenticator.AddCredentials(username, Passwords.createHash(password))
+            replyTo ! AccountCreated(username)
+          }
+      }
   }
 
-  def load[T](accounts: ActorRef[CreateAccount], askTimeout: FiniteDuration)
-             (implicit mat: Materializer,
-              scheduler: Scheduler
-             ): Flow[ByteString, CreateAccountReply, NotUsed] = {
-    implicit val timeout: Timeout = askTimeout
-    Flow[ByteString]
-      .via(Framing.delimiter(ByteString("\n"), 256, allowTruncation = true))
-      .map(_.utf8String)
-      .mapAsync(parallelism = 1)(accounts ? createAccount(_))
+  def eventHandler: (State, Event) => State = {
+    case (State(usernames), AccountCreated(username)) => State(usernames + username)
   }
 
   def createAccount(username: String, password: String)(replyTo: ActorRef[CreateAccountReply]): CreateAccount =
--- src/main/scala/rocks/heikoseeberger/chakka/iam/Main.scala
+++ src/main/scala/rocks/heikoseeberger/chakka/iam/Main.scala
@@ -1,27 +1,20 @@
 package rocks.heikoseeberger.chakka.iam
 
-import java.nio.file.Path
-
-import akka.actor.{CoordinatedShutdown, Scheduler}
 import akka.actor.CoordinatedShutdown.Reason
 import akka.actor.typed.scaladsl.Behaviors
-import akka.actor.typed.{ActorRef, ActorSystem, Behavior, Terminated}
+import akka.actor.typed.{ActorSystem, Behavior, Terminated}
+import akka.actor.{CoordinatedShutdown, Scheduler}
 import akka.stream.Materializer
-import akka.stream.scaladsl.FileIO
 import akka.stream.typed.ActorMaterializer
 import org.apache.logging.log4j.core.async.AsyncLoggerContextSelector
 import org.apache.logging.log4j.scala.Logging
 import pureconfig.loadConfigOrThrow
 
-import scala.concurrent.ExecutionContext
-import scala.concurrent.duration.FiniteDuration
-import scala.util.{Failure, Success}
-
 object Main extends Logging {
   import akka.actor.typed.scaladsl.adapter._
 
   sealed trait Command
-  final case class Config(initialAccounts: Path, askTimeout: FiniteDuration, accounts: Accounts.Config)
+  final case class Config(accounts: Accounts.Config, api: Api.Config)
 
   final case object TopLevelActorTerminated extends Reason
 
@@ -35,44 +28,26 @@ object Main extends Logging {
   }
 
   def apply(config: Config): Behavior[Command] = {
-    import config._
 
     Behaviors.setup { context =>
 
       implicit val mat: Materializer = ActorMaterializer()(context.system)
       implicit val scheduler: Scheduler = context.system.scheduler
 
-      val accounts = context.spawn(Accounts(config.accounts), "accounts")
-      context.watch(accounts)
-
       val authenticator = context.spawn(Authenticator(), "authenticator")
       context.watch(authenticator)
 
-      loadAccounts(initialAccounts, askTimeout, accounts)
+      val accounts = context.spawn(Accounts(config.accounts, authenticator), "accounts")
+      context.watch(accounts)
+
+      Api(config.api, accounts, authenticator)
 
       Behaviors.receiveSignal {
         case (_, Terminated(actor)) =>
           logger.error(s"Shutting down because $actor terminated")
           CoordinatedShutdown(context.system.toUntyped).run(TopLevelActorTerminated)
-          Behaviors.stopped
+          Behaviors.same
       }
     }
   }
-
-  private def loadAccounts(initialAccounts: Path,
-                           askTimeout: FiniteDuration,
-                           accounts: ActorRef[Accounts.Command]
-                          )(implicit mat: Materializer, scheduler: Scheduler, ec: ExecutionContext): Unit =
-    FileIO
-      .fromPath(initialAccounts)
-      .via(Accounts.load(accounts, askTimeout))
-      .runFold((0, 0, 0)) {
-        case ((i, t, c), Accounts.UsernameInvalid) => (i + 1, t, c)
-        case ((i, t, c), Accounts.UsernameTaken) => (i, t + 1, c)
-        case ((i, t, c), Accounts.AccountCreated(_)) => (i, t, c + 1)
-      }
-      .onComplete {
-        case Failure(cause) => logger.error("Cannot load accounts", cause)
-        case Success((i, t, c)) => logger.info(s"Accounts loaded: invalid=$i, taken:$t, created=$c")
-      }
 }
